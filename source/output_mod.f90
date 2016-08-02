1cf026bd50163d0e60dc95e98d0b64c6558aea9c
rwesson <rw@nebulousresearch.org>
2016-08-02 11:34:45 +0100

    corrected typo

--- source/output_mod.f90
+++ source/output_mod.f90
@@ -1306,7 +1306,7 @@ module output_mod
               end do
            end do
 
-           write(27,*) 'T(H+) ', TeVol(iAb,1,2), ' componenet ', iAb
+           write(27,*) 'T(H+) ', TeVol(iAb,1,2), ' component ', iAb
 
            write(30, *) "Element      Ion        <ion>/<H+>I     <ion>/<H+>II"
            write(30, *)
