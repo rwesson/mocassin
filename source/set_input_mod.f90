558e5ed22c2891efc1fa5d87d54c187c9a97a120
rwesson <rw@nebulousresearch.org>
2016-09-22 22:27:15 +0100

    fixed condition which prevented diffuse source models from running

--- source/set_input_mod.f90
+++ source/set_input_mod.f90
@@ -380,10 +380,6 @@ module set_input_mod
                 backspace 10
                 read(unit=10, fmt=*, iostat=ios) keyword, nPhotonsTot
                 !print*, keyword, nPhotonsTot
-                if (nPhotonsTot .eq. 0) then
-                  print*,'! readInput: no photons in model.'
-                  stop
-                endif
             case ("nx")
                 backspace 10
                 read(unit=10, fmt=*, iostat=ios) keyword, nxin(1)
@@ -573,6 +569,11 @@ module set_input_mod
          end do
 
         close(10)
+
+        if (nPhotonsTot .eq. 0 .and. nPhotonsDiffuse .eq. 0) then
+          print*,'! readInput: no photons in model.'
+          stop
+        endif
 
         allocate(lgDataAvailable(3:nElements, 1:nstages), stat=err)
         if (err /= 0) then
